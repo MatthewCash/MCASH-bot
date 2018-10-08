70a69edbb5f7907bf4c91ba30b818a251d32cc02
Matthew <matthew_cash@outlook.com>
2018-10-08 12:55:40 -0700

    Commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    console.log("web = gud")
+    console.log("web = gud");
   }).listen(9615);
 
 var bot = new Discord.Client;
