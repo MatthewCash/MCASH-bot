c9ca36a3794b29dd5413b0ea79870dd1bfaf00d6
Matthew <matthew_cash@outlook.com>
2018-10-08 12:47:48 -0700

    Commit

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 const Discord = require('discord.js');
 const fs = require('fs');
 
-var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
-var server_host = process.env.YOUR_HOST || '0.0.0.0';
-server.listen(server_port, server_host, function() {
-    console.log('Listening on port %d', server_port);
-});
+
 
 var bot = new Discord.Client;
 
