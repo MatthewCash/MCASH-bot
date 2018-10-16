70fab4e841145f97ed15f4a4d107ae4cfa639b8b
Matthew_Cash <matthew_cash@outlook.com>
2018-10-15 19:40:31 -0700

    Update index.js

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ bot.on('message', message => {
                     }
                 })
             case ">gay":
-                message.channel.send("@" + message.author.username + " you hella gay neighba :gay_pride_flag: ");
+                message.reply("you hella gay neighba :gay_pride_flag: 8/8 gayness alert");
 
         }
     }
