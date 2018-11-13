d716f21546f8b8cb2a541464eb7c0a4f58a62c00
Matthew <matthew_cash@outlook.com>
2018-11-12 17:48:41 -0800

    Updated MOTD embeds

--- main.js
+++ main.js
@@ -4,14 +4,13 @@ const fs = require('fs');
 const bot = new Discord.Client();
 let commandList = new Discord.Collection();
 let publicCommandList = new Discord.Collection();
+let embed = new Discord.RichEmbed();
 
 const commandPrefix = '>';
 
 //--------------------------------------------
 //----Load-Commands---------------------------
 //--------------------------------------------
-
-
 
 fs.readdir('./commands/', (err, data) => {
     if(err) console.error(err);
@@ -85,24 +84,14 @@ bot.on('message', async message => {
         message.channel.fetchMessages({ limit: 10 })
             .then(messages => {
                 if (messages.size == 1){                
-                    message.channel.send({
-                        'embed': {
-                            'title': 'Welcome!',
-                            'description': 'Responses are generally sent within `2 hours`!\rI get a lot of DMs, please read over the list below to see if your question has already been answered!',
-                            'color': 1993413,
-                            'footer': {
-                                'text': 'Created by Matthew_Cash#0210'
-                            },
-                            'author': {
-                                'name': 'Hello! Thank you for contacting me!',
-                                'icon_url': 'http://gravatar.com/avatar/2b152bb63629cff42dee60ed95409e19'
-                            },
-                            'fields': [{
-                                'name': 'Common Questions',
-                                'value': '```css\r[Can I get staff/partner on one of your servers?]\r- I am a developer on all of my servers, I would recommend checking if we have a currently applications process, or contacting a different staff member!\r\r[Can I hire you?]\r- Sure, I work as a developer and system administrator, let me know some more info, and I will get back to you ASAP!\r\r#General-Information\r- Timezone = PST\r- Languages = Java, C#, Bash, JS(Learning)\r- Current Servers = AstroHQ, ChampionPvP```'
-                            }]
-                        }
-                    });
+                    embed
+                        .setTitle('Welcome!')
+                        .setDescription('Responses are generally sent within `2 hours`!\rI get a lot of DMs, please read over the list below to see if your question has already been answered!')
+                        .setColor(1993413)
+                        .setFooter('Created by Matthew_Cash#0210')
+                        .setAuthor('Hello! Thank you for contacting me!', bot.user.avatarURL)
+                        .addField('Common Questions', '```css\r[Can I get staff/partner on one of your servers?]\r- I am a developer on all of my servers, I would recommend checking if we have a currently applications process, or contacting a different staff member!\r\r[Can I hire you?]\r- Sure, I work as a developer and system administrator, let me know some more info, and I will get back to you ASAP!\r\r#General-Information\r- Timezone = PST\r- Languages = Java, C#, Bash, JS(Learning)\r- Current Servers = AstroHQ, ChampionPvP```');
+                    message.channel.send(embed);
                 }    
             }
             )
