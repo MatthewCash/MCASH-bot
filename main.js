1688d22cd32fa7c0c657efae00239f7148558f56
Matthew <matthew_cash@outlook.com>
2018-11-13 18:53:58 -0800

    Updated command logger

--- main.js
+++ main.js
@@ -65,36 +65,33 @@ bot.on('message', async message => {
 
         if(publicCommandList.get(command)) {
             publicCommandList.get(command).execute(bot, sender, message, args);
-        }else{
-            if ((message.author == bot.user || message.channel.id === '508105216911212585') && commandList.get(command)){
-                commandList.get(command).execute(bot, sender, message, args);
-            }
-        }   
+            console.log([sender.username + sender.discriminator, command, args]);
+        }else if ((message.author == bot.user || message.channel.id === '508105216911212585') && commandList.get(command)){
+            commandList.get(command).execute(bot, sender, message, args);
+            console.log([sender.username + sender.discriminator, command, args]);
+        }        
     }
 
     if (message.channel.type == 'dm' || message.channel.type == 'group'){
-        console.log('Sending empty to user ' + message.channel.id);
-        bot.users.get('498881915756216322').send('Deleting...')
-            .then(msg => {
-                msg.delete(1);
-            });
+        bot.users.get('498881915756216322').send('Deleting...').then(msg => {
+            msg.delete(1);
+        });
     }
 
     if (message.channel.type == 'dm' && message.author != bot.user) {
-        message.channel.fetchMessages({ limit: 10 })
-            .then(messages => {
-                if (messages.size == 1){                
-                    embed
-                        .setTitle('Welcome!')
-                        .setDescription('Responses are generally sent within `2 hours`!\rI get a lot of DMs, please read over the list below to see if your question has already been answered!')
-                        .setColor(1993413)
-                        .setFooter('Created by Matthew_Cash#0210')
-                        .setAuthor('Hello! Thank you for contacting me!', bot.user.avatarURL)
-                        .addField('Common Questions', '```css\r[Can I get staff/partner on one of your servers?]\r- I am a developer on all of my servers, I would recommend checking if we have a currently applications process, or contacting a different staff member!\r\r[Can I hire you?]\r- Sure, I work as a developer and system administrator, let me know some more info, and I will get back to you ASAP!\r\r#General-Information\r- Timezone = PST\r- Languages = Java, C#, Bash, JS(Learning)\r- Current Servers = AstroHQ, ChampionPvP```');
-                    message.channel.send(embed);
-                }    
-            }
-            )
+        message.channel.fetchMessages({ limit: 10 }).then(messages => {
+            if (messages.size == 1){                
+                embed
+                    .setTitle('Welcome!')
+                    .setDescription('Responses are generally sent within `2 hours`!\rI get a lot of DMs, please read over the list below to see if your question has already been answered!')
+                    .setColor(1993413)
+                    .setFooter('Created by Matthew_Cash#0210')
+                    .setAuthor('Hello! Thank you for contacting me!', bot.user.avatarURL)
+                    .addField('Common Questions', '```css\r[Can I get staff/partner on one of your servers?]\r- I am a developer on all of my servers, I would recommend checking if we have a currently applications process, or contacting a different staff member!\r\r[Can I hire you?]\r- Sure, I work as a developer and system administrator, let me know some more info, and I will get back to you ASAP!\r\r#General-Information\r- Timezone = PST\r- Languages = Java, C#, Bash, JS(Learning)\r- Current Servers = AstroHQ, ChampionPvP```');
+                message.channel.send(embed);
+            }    
+        }
+        )
             .catch(console.error);
     }
 });
