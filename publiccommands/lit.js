3720e46a4df307a2824a7b669dc3cf4bd74e6481
Matthew <matthew_cash@outlook.com>
2018-11-30 17:49:14 -0800

    Fix Tagging

--- publiccommands/lit.js
+++ publiccommands/lit.js
@@ -1,10 +1,10 @@
 async function execute(bot, sender, message, args) {
     
-    if (args.length <= 0 || message.channel.type !== 'text'){
+    if (args.length <= 0){
         return message.channel.send('<@' + message.author.id + '> you hella lit neighba :fire: 8/8 litness alert');
     }
 
-    let tagged = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]) || message.channel.members.get(args[0]));
+    let tagged = message.mentions.users.first() || message.guild.members.get(args[0]) || message.channel.members.get(args[0]);
     if (tagged) {
         message.channel.send('<@' + tagged.id + '> you hella lit neighba :fire: 8/8 litness alert');        
     }else message.channel.send(':fire: Yeah.. i cant find that user, litness 0/0 D: ');    
